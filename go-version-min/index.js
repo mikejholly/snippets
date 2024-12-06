cccbf9f659d80ebba8bd962077edafee28391b3d
Mike Holly <mikejholly@gmail.com>
2024-12-06 10:43:17 -0800

    Update

--- go-version-min/index.js
+++ go-version-min/index.js
@@ -1,2 +1,21 @@
 
-console.log(JSON.stringify({foo: "bar"}));
+let assertions = [
+  {
+    op: "equals",
+    path: "path.in.md",
+    args: ["value"],
+    ok: true,
+    severity: "ok",
+    details: "Does the specified metadata field equal 'value'",
+  },
+  {
+    op: "contains",
+    path: "path.in.other",
+    args: [{foo: 1}],
+    ok: false,
+    severity: "warn",
+    details: "Does the path contain the object",
+  }
+];
+
+console.log(JSON.stringify(assertions));
